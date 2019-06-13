25db2cb9e22cd18ee870b3ae6d5ad8dc3fb39dd7
Skip <skiphog@mail.ru>
2019-06-13 23:05:10 +0300

    change login template

--- templates/auth/login.php
+++ templates/auth/login.php
@@ -14,22 +14,21 @@ $auth = auth();
 <?php $this->stop(); ?>
 
 <?php $this->start('content'); ?>
-    <h1>Вход на сайт</h1>
+    <h1 class="text-center">Вход на сайт</h1>
 
-    <form class="information" method="post" action="<?php echo url('/authentication'); ?>">
-        <ul class="information-list">
-            <li class="information-list-item mb-10">
-                <input class="information-list-input" name="login" type="text" placeholder="Логин" required>
-            </li>
-            <li class="information-list-item mb-10">
-                <input class="information-list-input" name="password" type="password" placeholder="Пароль" required>
-            </li>
-            <li class="information-list-item mb-10">
-                <input id="check-1" name="save" type="checkbox" value="1" checked>
-                <label for="check-1">Запомнить меня</label>
-            </li>
-            <li><button class="btn btn-default" type="submit">Войти</button></li>
-        </ul>
+    <form method="post" action="<?php echo url('/authentication'); ?>">
+
+        <div>
+            <input name="login" type="text">
+        </div>
+
+        <div>
+            <input name="password" type="password">
+        </div>
+
+        <div>
+            <button class="btn btn-default" type="submit">Войти</button>
+        </div>
     </form>
 <?php $this->stop(); ?>
 
