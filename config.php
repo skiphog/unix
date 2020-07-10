6169ce39968babf8e09953726488b3734ca9d686
Skip <skiphog@gmail.com>
2020-07-10 15:55:55 +0300

    change nginx settings

--- config.php
+++ config.php
@@ -28,7 +28,7 @@ return [
         'host'     => '127.0.0.1',
         'dbname'   => 'swingsite',
         'username' => 'root',
-        'password' => '',
+        'password' => 'root',
         'options'  => [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
